c287d83cd15f37ebc2d4fed10d45ab82210272a1
VivienIngrams <vivingrams@gmail.com>
2023-09-19 10:50:34 +0100

    Initial Project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        pt: ["var(--font-pt-sans)"],
+        playfair: ["var(--font-playfair-display)"],
+        moda: ["var(--font-bodoni-moda)"],
       },
     },
   },
