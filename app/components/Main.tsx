a304c9e96ff5efdf5dd33dd02a447a575998fa20
VivienIngrams <vivingrams@gmail.com>
2023-09-26 16:49:57 +0100

    Editing Main component

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -5,27 +5,29 @@ import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const Main = () => {
   return (
-    <div id='home' className="z-[2] w-full h-screen flex items-center justify-center text-center">
-    <div className="max-w-[1240px] p-2">
-      <div className=" text-xl font-raj tracking-widest text-slate-600 mt-5">
+    <div id="home" className="z-[2] w-full h-screen ">
+      <div className="h-[90%] flex items-center justify-center text-center">
+        <div className="max-w-[1240px] p-2">
+          <div className=" text-xl font-raj tracking-widest text-slate-600 mt-5">
             <p> Coding with Creativity for Dynamic Web Experiences</p>
-          </div>
-          <div className="flex items-center justify-between max-w-[330px] m-auto py-8">
-            <div className="rounded-full shadow-md shadow-neutral-500 p-2 cursor-pointer hover:scale-105 ease-in duration-300">
-            <a href="https://www.linkedin.com/in/vivien-ingrams"><FaLinkedinIn /></a>
-            </div>
-            <div className="rounded-full shadow-md shadow-neutral-500 p-2 cursor-pointer hover:scale-105 ease-in duration-300">
-            <a href="https://github.com/VivienIngrams"><FaGithub /></a>
-            </div>
-            <div className="rounded-full shadow-md shadow-neutral-500 p-2 cursor-pointer hover:scale-105 ease-in duration-300">
-            <a href="mailto:vivieningrams@hotmail.com" target="_blank"><AiOutlineMail /></a>
-            </div>
-            <div className="rounded-full shadow-md shadow-neutral-500 p-2 cursor-pointer hover:scale-105 ease-in duration-300">
-            <Link href="/#contact"><BsFillPersonLinesFill /></Link>
-            </div>
           </div>
         </div>
       </div>
+      <div className="flex items-center justify-between max-w-[330px] m-auto">
+            <div className="rounded-full shadow-md shadow-neutral-500 p-2 cursor-pointer hover:scale-105 ease-in duration-300">
+              <FaLinkedinIn />
+            </div>
+            <div className="rounded-full shadow-md shadow-neutral-500 p-2 cursor-pointer hover:scale-105 ease-in duration-300">
+              <FaGithub />
+            </div>
+            <div className="rounded-full shadow-md shadow-neutral-500 p-2 cursor-pointer hover:scale-105 ease-in duration-300">
+              <AiOutlineMail />
+            </div>
+            <div className="rounded-full shadow-md shadow-neutral-500 p-2 cursor-pointer hover:scale-105 ease-in duration-300">
+              <BsFillPersonLinesFill />
+            </div>
+          </div>
+    </div>
   );
 };
 
