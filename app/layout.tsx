41648fee9ec40acdbd77d48a7ed1c9a1b4db019a
VivienIngrams <vivingrams@gmail.com>
2024-04-03 16:10:41 +0100

    Minor edits

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`min-h-screen bg-[#f9f3e2] ${pt.variable} ${raj.variable} ${poiret.variable}`}
+        className={`min-h-screen bg-[#ded8ca] ${pt.variable} ${raj.variable} ${poiret.variable}`}
       >
         <NavBar />
         <Image
