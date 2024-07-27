29603b4aa70e7b13f53c393da22e26316d2c0ee2
VivienIngrams <vivingrams@gmail.com>
2024-07-27 17:29:38 +0100

    ADjustments links

--- app/model-portfolio/page.tsx
+++ app/model-portfolio/page.tsx
@@ -32,7 +32,7 @@ const ModelPortfolio: React.FC = () => {
           <button className="px-8 py-2 mt-4 mr-8">Code</button>
         </a>
         <a
-          href="https://https://sakiko-oishi.vercel.app/"
+          href="https://sakiko-oishi.vercel.app/"
           target="_blank"
           rel="noreferrer"
         >
