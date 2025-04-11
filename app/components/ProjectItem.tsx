8d883dc5d5e3803a5f1f7313da31fb1bdbf39097
Vivien <vivingrams@gmail.com>
2025-04-11 15:14:12 +0100

    Mobile photos

--- app/components/ProjectItem.tsx
+++ app/components/ProjectItem.tsx
@@ -3,32 +3,58 @@ import Link from "next/link";
 
 interface ProjectItemProps {
   title: string;
+  backgroundImgMobile: string;
   backgroundImg: string;
-  tech: string;
   projectUrl: string;
+  demoUrl: string;
 }
 
 const ProjectItem: React.FC<ProjectItemProps> = ({
   title,
+  backgroundImgMobile,
   backgroundImg,
-  tech,
   projectUrl,
+  demoUrl,
 }) => {
   return (
-    <div className="relative flex item-center justify-center h-auto w-full shadow-md shadow-stone-400 border-2 rounded border-stone-400 group hover:bg-gradient-to-r from-[black] to-red-800">
-      <Image
-        className=" group-hover:opacity-20 rounded"
-        src={backgroundImg}
-        alt="Website project"
-        height={500}
-        width={1000}
-      />
+    <div className="relative flex items-center justify-center h-auto w-full shadow-md shadow-stone-400 border-2 rounded border-stone-400 group hover:bg-gradient-to-r from-[black] to-red-800">
+      {/* Use different images for small and larger screens */}
+      <div className="block sm:hidden relative w-full h-[600px]">
+  <Image
+    className="group-hover:opacity-20 rounded object-fill"
+    src={backgroundImgMobile}
+    alt="Website project"
+    layout="fill"
+  />
+</div>
+      <div className="hidden sm:block">
+        <Image
+          className="group-hover:opacity-20 rounded"
+          src={backgroundImg}
+          alt="Website project"
+          height={500} // Landscape orientation for larger screens
+          width={1000}
+        />
+      </div>
       <div className="hidden font-raj group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
         <Link href={projectUrl}>
           <h4 className="text-lg md:text-xl lg:text-3xl 2xl:text-4xl font-poiret font-bold uppercase tracking-wide text-white text-center whitespace-nowrap overflow-hidden text-ellipsis">
             {title}
           </h4>
         </Link>
+        {/* Flex container for horizontal alignment */}
+        <div className="mt-4 flex flex-col md:flex-row items-center justify-center gap-4">
+          <Link href={demoUrl}>
+            <div className="px-4 py-2 rounded font-semibold bg-white/80 hover:bg-white text-red-900 text-sm md:text-base shadow-md transition-all duration-200">
+              Visit Site
+            </div>
+          </Link>
+          <Link href={projectUrl}>
+            <div className="px-4 py-2 rounded backdrop-blur bg-transparent border border-white/70 hover:bg-white hover:text-slate-800 text-white text-sm md:text-base shadow-md transition-all duration-200">
+              More Info
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
   );
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,81 +1,24 @@
 import ProjectItem from "./ProjectItem";
-import Image from "next/image";
-
-const backgroundImgLight =
-  "https://images.unsplash.com/photo-1608114759160-ad0b1bac35b2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1936&q=80";
+import { projects } from "../projectsData";
 
 const Projects: React.FC = () => {
   return (
-    <div
-      id="projects"
-      className="w-full py-24 relative "
-    >
-          
-      <div className="md:max-w-[75vw] xl:max-w-[1000px]  mx-auto px-4 py-4">
+    <div id="projects" className="w-full py-24 relative">
+      <div className="md:max-w-[75vw] xl:max-w-[1000px] mx-auto px-4 py-4">
         <p className="mb-16 text-center font-poiret font-bold text-3xl md:text-4xl uppercase tracking-widest">
           Projects
         </p>
         <div className="grid gap-8 md:gap-16">
-          <ProjectItem
-            title="Media Art Research"
-            backgroundImg="/assets/Projects/ne.png"
-            tech="Next JS"
-            projectUrl="/ne"
-          />
-          <ProjectItem
-            title="Photographer"
-            backgroundImg="/assets/Projects/joel.png"
-            tech="Next JS"
-            projectUrl="/joel"
-          />
-          <ProjectItem
-            title="Art Photography"
-            backgroundImg="/assets/Projects/mathilde1.png"
-            tech="Next JS"
-            projectUrl="/mathilde"
-          />
-          <ProjectItem
-            title="Art District App"
-            backgroundImg="/assets/Projects/bombarda.png"
-            tech="Next JS"
-            projectUrl="/bombarda"
-          />
-          <ProjectItem
-            title="Photography and art gallery"
-            backgroundImg="/assets/Projects/2regards.png"
-            tech="Next JS"
-            projectUrl="/photography"
-          />
-          <ProjectItem
-            title="Psychologist's professional website"
-            backgroundImg="/assets/Projects/Marta.png"
-            tech="Next JS"
-            projectUrl="/psychologist"
-          />
-          <ProjectItem
-            title="Research blog"
-            backgroundImg="/assets/Projects/AlexSiteResearch1.png"
-            tech="Next JS"
-            projectUrl="/research-blog"
-          />
-          <ProjectItem
-            title="Model Portfolio"
-            backgroundImg="/assets/Projects/KikoSite.png"
-            tech="Next JS"
-            projectUrl="/model-portfolio"
-          />
-          <ProjectItem
-            title="Art Facilitation"
-            backgroundImg="/assets/Projects/CelesteSiteWorkshops.png"
-            tech="Wordpress"
-            projectUrl="/art-facilitation"
-          />
-          <ProjectItem
-            title="Landing Page"
-            backgroundImg="/assets/Projects/LandingPage.png"
-            tech="Next JS"
-            projectUrl="/landing-page"
-          />
+          {projects.map((project) => (
+            <ProjectItem
+              key={project.slug}
+              title={project.title}
+              backgroundImg={project.imgUrl2}
+              backgroundImgMobile={project.img}
+              projectUrl={`/${project.slug}`}
+              demoUrl={project.demoUrl}
+            />
+          ))}
         </div>
       </div>
     </div>
